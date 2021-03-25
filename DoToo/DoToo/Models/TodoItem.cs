a3c321e60f5ef4120eef80bdae88234a5b2625c4
Paul de Mast <pjf.demast@avans.nl>
2021-03-25 15:58:17 +0100

    Removed MainPage (to use mvvm later on) Added Models folder Added Model TodoItem

---
+++ DoToo/DoToo/Models/TodoItem.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DoToo.Models {
+    class TodoItem {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public bool Completed { get; set; }
+        public DateTime Due { get; set; }
+    }
+}
