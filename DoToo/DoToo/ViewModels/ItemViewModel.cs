e97e6aee82b84ab6940f6b38a150680a1023536b
Paul de Mast <pjf.demast@avans.nl>
2021-04-07 14:40:46 +0200

    Binding ListView in MainView

--- DoToo/DoToo/ViewModels/ItemViewModel.cs
+++ DoToo/DoToo/ViewModels/ItemViewModel.cs
@@ -1,14 +1,24 @@
-﻿using DoToo.Repositories;
+﻿using DoToo.Models;
+using DoToo.Repositories;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
 
 namespace DoToo.ViewModels {
     public class ItemViewModel : ViewModel {
         private readonly TodoItemRepository repository;
 
+        public TodoItem Item { get; set; }
         public ItemViewModel(TodoItemRepository repository) {
             this.repository = repository;
+            Item = new TodoItem() { Due = DateTime.Now.AddDays(1) };
         }
+
+        public ICommand Save => new Command(async () => {
+            await repository.AddOrUpdateItem(Item);
+            await Navigation.PopAsync();
+        });
     }
 }
--- DoToo/DoToo/ViewModels/MainViewModel.cs
+++ DoToo/DoToo/ViewModels/MainViewModel.cs
@@ -1,7 +1,10 @@
-﻿using DoToo.Repositories;
+﻿using DoToo.Models;
+using DoToo.Repositories;
 using DoToo.Views;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
@@ -10,13 +13,32 @@ namespace DoToo.ViewModels {
     public class MainViewModel : ViewModel {
         private readonly TodoItemRepository repository;
 
+        public ObservableCollection<TodoItemViewModel> Items { get; set; }
+
         public MainViewModel(TodoItemRepository repository) {
+
+            repository.OnItemAdded += (sender, item) =>
+                Items.Add(CreateTodoItemViewModel(item));
+            repository.OnItemUpdated += (sender, item) =>
+                Task.Run(async () => await LoadData());
             this.repository = repository;
             Task.Run(async () => await LoadData());
         }
 
-        private Task LoadData() {
-            throw new NotImplementedException();
+        private async Task LoadData() {
+            var items = await repository.GetItems();
+            var itemViewModels = items.Select(
+                i => CreateTodoItemViewModel(i));
+            Items = new ObservableCollection<TodoItemViewModel>(itemViewModels);
+        }
+
+        private TodoItemViewModel CreateTodoItemViewModel(TodoItem item) {
+            var itemViewModel = new TodoItemViewModel(item);
+            itemViewModel.ItemStatusChanged += ItemStatusChanged;
+            return itemViewModel;
+        }
+
+        private void ItemStatusChanged(object sender, EventArgs e) {
         }
 
         public ICommand AddItem =>
